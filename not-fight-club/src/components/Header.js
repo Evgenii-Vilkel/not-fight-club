db261bb299a0592a0298ea036ed866b9b74d5d47
Evgenii-Vilkel <evgeniivilkel@gmail.com>
2025-08-18 20:44:26 +0300

    feat: add react router and header

---
+++ not-fight-club/src/components/Header.js
@@ -0,0 +1,15 @@
+function Header() {
+  return (
+    <>
+        <nav>
+            <ul>
+                <li><a href="/">Home</a></li>
+                <li><a href="/сharacter">Character</a></li>
+                <li><a href="/ыettings">Settings</a></li>
+            </ul>
+        </nav>
+    </>
+  );
+}
+
+export default Header;
